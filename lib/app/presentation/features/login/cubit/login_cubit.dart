e45b9e2927e0e852065a450de7f6db8c0db67897
mirza-d3x <mohammedmirsa010@gmail.com>
2024-09-24 15:16:54 +0530

    login completed

--- lib/app/presentation/features/login/cubit/login_cubit.dart
+++ lib/app/presentation/features/login/cubit/login_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:erp_mobile/app/repository/modesl/response_models/verify_user_model.dart';
+import 'package:erp_mobile/app/widgets/snackbar/custom_snackbar.dart';
 import 'package:erp_mobile/services/service_locator.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +9,16 @@ part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   final ServiceLocator _serviceLocator;
-  LoginCubit(this._serviceLocator) : super(LoginInitial()) {
+  LoginCubit(this._serviceLocator)
+      : super(const LoginInitial(
+            isPasswordFieldEnabled: false,
+            isPasswordVerified: false,
+            isPasswordVerifyEnabled: false,
+            isUserNameVerifyEnabled: false,
+            selectedBranches: '',
+            branches: [],
+            financialYears: [],
+            selectedFinancialYeaers: '')) {
     _init();
   }
 
@@ -16,6 +27,16 @@ class LoginCubit extends Cubit<LoginState> {
   late final TextEditingController branchController;
   late final TextEditingController yearController;
 
+  bool _isUserNameVerifyEnabled = false;
+  bool _isPasswordFieldEnabled = false;
+  bool _isPasswordVerifyEnabled = false;
+  bool _isPasswordVerified = false;
+
+  String _selectedBranch = '';
+  String _selectedYears = '';
+  final List<String> _userBranches = [];
+  final List<String> _financialYears = [];
+
   void _init() {
     userNameController = TextEditingController();
     passwordController = TextEditingController();
@@ -23,7 +44,119 @@ class LoginCubit extends Cubit<LoginState> {
     yearController = TextEditingController();
   }
 
-  void onSignin() {}
+  void onUserNameChanged(String value) {
+    if (value.length >= 3) {
+      _isUserNameVerifyEnabled = true;
+      emitState();
+    }
+  }
+
+  void onPasswordChanged(String value) {
+    if (value.length >= 3) {
+      _isPasswordVerifyEnabled = true;
+      emitState();
+    }
+  }
+
+  void onSignin(BuildContext context) async {
+    final verifyUser = await _serviceLocator.apiService
+        .sendBranchMasterRequest(branchName: _selectedBranch);
+
+    if (verifyUser.status == 'Success') {
+    } else {
+      showCustomSnackbar(context, verifyUser.message);
+    }
+  }
+
+  void onVerifyUsername(BuildContext context) async {
+    final verifyUser = await _serviceLocator.apiService
+        .sendVerifyUserRequest(userName: userNameController.text.trim());
+
+    if (verifyUser.status == 'Success') {
+      _isPasswordFieldEnabled = true;
+      emitState();
+    } else {
+      showCustomSnackbar(context, verifyUser.message);
+    }
+  }
+
+  void onVerifyPassword(BuildContext context) async {
+    final verifyUser = await _serviceLocator.apiService
+        .sendVerifyPasswordRequest(
+            userName: userNameController.text.trim(),
+            password: passwordController.text.trim());
+
+    if (verifyUser.status == 'Success') {
+      _isPasswordVerified = true;
+      await getUserBranches(context);
+      emitState();
+    } else {
+      showCustomSnackbar(context, verifyUser.message);
+    }
+  }
+
+  Future getUserBranches(BuildContext context) async {
+    final verifyUser = await _serviceLocator.apiService
+        .sendUserBranchesRequest(userName: userNameController.text.trim());
+
+    if (verifyUser.status == 'Success') {
+      if (verifyUser.responseList != null) {
+        _userBranches.addAll(verifyUser.responseList!.map(
+          (e) => e.branchCode,
+        ));
+        _selectedBranch = _userBranches.first;
+        await getFinancialYears(context);
+      }
+
+      emitState();
+    } else {
+      showCustomSnackbar(context, verifyUser.message);
+    }
+  }
+
+  Future getFinancialYears(BuildContext context) async {
+    final verifyUser = await _serviceLocator.apiService
+        .sendFinancialYearRequest(
+            userName: userNameController.text.trim(), branch: _selectedBranch);
+
+    if (verifyUser.status == 'Success') {
+      if (verifyUser.responseList != null) {
+        _financialYears.addAll(verifyUser.responseList!.map(
+          (e) => e.fyearcode,
+        ));
+        _selectedYears = _financialYears.first;
+      }
+
+      emitState();
+    } else {
+      showCustomSnackbar(context, verifyUser.message);
+    }
+  }
+
+  void selectBranch(String? branch) {
+    _selectedBranch = branch ?? _selectedBranch;
+    emitState();
+  }
+
+  void selectYear(String? year) {
+    _selectedYears = year ?? _selectedYears;
+    emitState();
+  }
+
+  emitState() {
+    emit(LoginTemp());
+    emit(
+      LoginInitial(
+          isPasswordFieldEnabled: _isPasswordFieldEnabled,
+          isPasswordVerified: _isPasswordVerified,
+          isPasswordVerifyEnabled: _isPasswordVerifyEnabled,
+          isUserNameVerifyEnabled: _isUserNameVerifyEnabled,
+          branches: _userBranches,
+          selectedBranches: _selectedBranch,
+          financialYears: _financialYears,
+          selectedFinancialYeaers: _selectedYears),
+    );
+  }
 
   @override
   Future<void> close() {
--- lib/app/presentation/features/login/ui/login_screen.dart
+++ lib/app/presentation/features/login/ui/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:erp_mobile/app/presentation/features/login/cubit/login_cubit.dart';
 import 'package:erp_mobile/app/widgets/custom_button.dart';
+import 'package:erp_mobile/app/widgets/custom_dropdown.dart';
 import 'package:erp_mobile/app/widgets/custom_text_field.dart';
 import 'package:erp_mobile/constants/assets/image_assets.dart';
 import 'package:erp_mobile/constants/theme/styles.dart';
@@ -14,72 +15,146 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     final cubit = BlocProvider.of<LoginCubit>(context);
+
     return Scaffold(
-      body: Container(
-        height: size.height,
-        width: size.width,
-        color: blue100,
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              AssetImages.splashLogo,
-              // height: 250.sp,
-              // width: 250.sp,
-            ),
-            SizedBox(height: 16.h),
-            Text(
-              "Login",
-              style: customTextStyle(
-                fontStyle: FontStyle.headLineSmall,
-                color: FontColor.fontPrimary,
-              ),
-            ),
-            SizedBox(height: 16.h),
-            PhysicalModel(
+      body: BlocBuilder<LoginCubit, LoginState>(
+        builder: (context, state) {
+          if (state is LoginInitial) {
+            return Container(
+              height: size.height,
+              width: size.width,
               color: blue100,
-              elevation: 5,
-              child: Container(
-                width: size.width * .7,
-                padding: EdgeInsets.all(16.sp),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(5.sp),
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(AssetImages.splashLogo),
+                  SizedBox(height: 16.h),
+                  Text(
+                    "Login",
+                    style: customTextStyle(
+                      fontStyle: FontStyle.headLineSmall,
+                      color: FontColor.fontPrimary,
+                    ),
                   ),
-                ),
-                child: Column(
-                  children: [
-                    CustomTextField(
-                      controller: cubit.userNameController,
-                      labelText: "Username",
+                  SizedBox(height: 16.h),
+                  PhysicalModel(
+                    color: blue100,
+                    elevation: 5,
+                    child: Container(
+                      width: size.width * .7,
+                      padding: EdgeInsets.all(16.sp),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.sp),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: CustomTextField(
+                                  controller: cubit.userNameController,
+                                  labelText: "Username",
+                                  onChanged: (value) {
+                                    cubit.onUserNameChanged(value!);
+                                    return null;
+                                  },
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: state.isUserNameVerifyEnabled
+                                    ? () {
+                                        cubit.onVerifyUsername(context);
+                                      }
+                                    : null,
+                                style: TextButton.styleFrom(
+                                  foregroundColor: state.isUserNameVerifyEnabled
+                                      ? Colors.blue
+                                      : Colors.grey,
+                                ),
+                                child: Text(!state.isPasswordFieldEnabled
+                                    ? 'Verify'
+                                    : 'Verified'),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 16.h),
+                          AbsorbPointer(
+                            absorbing: !state.isPasswordFieldEnabled,
+                            child: Opacity(
+                              opacity: state.isPasswordFieldEnabled ? 1 : 0.5,
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: CustomTextField(
+                                      controller: cubit.passwordController,
+                                      labelText: "Password",
+                                      onChanged: (value) {
+                                        cubit.onPasswordChanged(value!);
+                                        return null;
+                                      },
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: state.isPasswordVerifyEnabled
+                                        ? () {
+                                            cubit.onVerifyPassword(context);
+                                          }
+                                        : null,
+                                    style: TextButton.styleFrom(
+                                      foregroundColor:
+                                          state.isPasswordVerifyEnabled
+                                              ? Colors.blue
+                                              : Colors.grey,
+                                    ),
+                                    child: Text(state.isPasswordVerified
+                                        ? 'Verified'
+                                        : 'Verify'),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 16.h),
+                          AbsorbPointer(
+                            absorbing: !state.isPasswordVerified,
+                            child: Opacity(
+                              opacity: state.isPasswordVerified ? 1 : 0.5,
+                              child: Column(
+                                children: [
+                                  CustomDropdownPicker(
+                                    items: state.branches,
+                                    selectedValue: state.selectedBranches,
+                                    onChanged: cubit.selectBranch,
+                                  ),
+                                  SizedBox(height: 16.h),
+                                  CustomDropdownPicker(
+                                    items: state.financialYears,
+                                    selectedValue:
+                                        state.selectedFinancialYeaers,
+                                    onChanged: cubit.selectYear,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 16.h),
+                          CustomElevatedButton(
+                            text: "Sign In",
+                            onPressed: state.isPasswordVerified ? () {} : null,
+                          ),
+                        ],
+                      ),
                     ),
-                    SizedBox(height: 16.h),
-                    CustomTextField(
-                      controller: cubit.passwordController,
-                      labelText: "Password",
-                    ),
-                    SizedBox(height: 16.h),
-                    CustomTextField(
-                      controller: cubit.branchController,
-                      labelText: "Branch",
-                    ),
-                    SizedBox(height: 16.h),
-                    CustomTextField(
-                      controller: cubit.yearController,
-                      labelText: "Year",
-                    ),
-                    SizedBox(height: 16.h),
-                    CustomElevatedButton(
-                      text: "Sign  In",
-                      onPressed: cubit.onSignin,
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            )
-          ],
-        ),
+            );
+          }
+          return Container();
+        },
       ),
     );
   }
