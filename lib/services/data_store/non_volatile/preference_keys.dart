38dd3741b901efda529f512fdd8b4fcf9c3655c9
Mirza <mohammedmirsa010@gmail.com>
2024-12-07 23:50:36 +0530

    preferences setup for data saving

---
+++ lib/services/data_store/non_volatile/preference_keys.dart
@@ -0,0 +1,6 @@
+class PreferenceKeys {
+  static const userName = 'userName';
+  static const groupName = 'groupName';
+  static const userBranch = 'userBranch';
+  static const userYear = 'userYear';
+}
